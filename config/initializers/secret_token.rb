d0136ff8e46ebd9e5fabfb2fa54dc9083ad0219a
Diana Marcela Jimenez Rojas <dimajira@gmail.com>
2013-02-03 22:58:13 -0500

    probando

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PesoIdeal::Application.config.secret_token = '986bef943899dc8227a0ec99b4f6f26fa1052a2907e4b2b519c81667ded0d15ab88733ec4a5edf2f1d902daabcbf75f27937b44e0f76e33d23afb0a26b7188a3'
