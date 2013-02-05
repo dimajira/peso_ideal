c998e04f26cd6abd83711941667ac4e8285600a1
Diana Marcela Jimenez Rojas <dimajira@gmail.com>
2013-02-04 22:39:39 -0500

    Historias de usuario referentes a ingreso de datos

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 PesoIdeal::Application.routes.draw do
+  root :to => "datos#index"
+  resources :datos
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -55,4 +59,5 @@ PesoIdeal::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+  
 end
